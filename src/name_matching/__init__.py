27e2185566141a00e9ed93c225645f1276f42816
Sri Krishna Chekuri <cskcvarma13@gmail.com>
2025-06-05 14:42:55 -0400

    Add loader for fake names

--- src/name_matching/__init__.py
+++ src/name_matching/__init__.py
@@ -1,6 +1,7 @@
 """Public package exports for name_matching."""
 
 from .db import ChromaDB
+from .loader import load_fake_names
 from .matcher import NameMatcher
 
-__all__ = ["ChromaDB", "NameMatcher"]
+__all__ = ["ChromaDB", "NameMatcher", "load_fake_names"]
